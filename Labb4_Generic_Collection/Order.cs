685a67955af1e0344da82298578cea3297c1a95c
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-12 14:05:52 +0100

    Working on details

--- Labb4_Generic_Collection/Order.cs
+++ Labb4_Generic_Collection/Order.cs
@@ -2,6 +2,7 @@
 {
     internal class Order
     {
+        //Private fields
         static int orderIdCounter = 1;
         private int _orderId;
         private List<MenuItem> _orderItems;
@@ -15,6 +16,12 @@
             _orderItems = orderItems;
             _tableNumber = tableNumber;
         }
+        public void AddMenuItem(MenuItem menuItem)
+        {
+            _orderItems.Add(menuItem);
+        }
+        
+        //Override to display orderID, tableNo, items ordered and total cost of order.
         public override string ToString()
         {
             decimal cost = 0;
@@ -22,8 +29,10 @@
             {
                 cost += costs.Price;
             }
-            var totalItems = string.Join(" - ", _orderItems);
-            return $"OrderId: {_orderId}\nTablenumber: {_tableNumber}\nOrder: {totalItems}\nTotal bill: {cost.ToString("C")}\n";
+            
+            //String.join to create a clear overview with all parameters of each order
+            string totalItems = string.Join(" - 1, ", _orderItems);
+            return $"Ordernumber: {_orderId}\n------------\nTablenumber: {_tableNumber}\nOrder: 1, {totalItems}\nTotal bill: {cost.ToString("C")}\n";            
         }
 
     }
